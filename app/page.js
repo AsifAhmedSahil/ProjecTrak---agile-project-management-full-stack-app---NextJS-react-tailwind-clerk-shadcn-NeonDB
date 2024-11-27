661051bae1c774293fddbcf1266083602331d0a9
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-11-27 12:42:03 +0600

    hero section design:

--- app/page.js
+++ app/page.js
@@ -7,11 +7,11 @@ export default function Home() {
     <div className="min-h-screen">
       {/* hero section */}
       <section className="container mx-auto text-center py-20">
-        <h1 className="text-6xl sm:text-7xl lg:text-8xl font-extrabold pb-6 flex flex-col gradient-title">
+        <h1 className="text-6xl sm:text-7xl lg:text-8xl font-extrabold pb-6 flex flex-col gradient-title pt-10">
           Transform Your Workflow <br />
           <span>with ProjecTrak</span>
         </h1>
-        <p>Empower your team with our intuitive project management solution.</p>
+        <p className="text-xl font-semibold max-w-3xl mx-auto mb-10 text-gray-300 pt-5">Empower your team with our intuitive project management solution.</p>
 
         <Link href={"/onboarding"}>
           <Button size="lg" className="mr-4">
