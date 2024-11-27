4de67d4068ca2ccc8b36c4d1de2ca2083b5fb196
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-11-28 00:00:45 +0600

    update

--- app/page.js
+++ app/page.js
@@ -62,7 +62,6 @@ export default function Home() {
                     {feature.title}
                   </h4>
                   <p className="text-gray-300">{feature.description}</p>
-                  <p className="text-gray-300">{feature.description}</p>
                 </CardContent>
               </Card>
             ))}
