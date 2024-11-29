b16414acc6666b14183b7f304c23625bfa8cb0af
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-11-29 09:27:57 +0600

    company carousel section:

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import CompanyCarousel from "@/components/company-carousel";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { BarChart, Calendar, ChevronRight, Layout } from "lucide-react";
@@ -66,6 +67,15 @@ export default function Home() {
               </Card>
             ))}
           </div>
+        </div>
+      </section>
+      {/* ---------------------------------------- */}
+
+      <section  className=" py-20 ">
+        <div className="container mx-auto">
+          <h3 className="text-3xl font-bold mb-12 text-center">Trusted by Industry Leaders</h3>
+            <CompanyCarousel/>
+          
         </div>
       </section>
     </div>
