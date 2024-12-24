f326adcf5f4dbad3aaa223737ad94bce3e8fcf5a
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-12-24 23:43:14 +0600

    update issue

--- app/layout.js
+++ app/layout.js
@@ -7,9 +7,6 @@ import { shadesOfPurple } from "@clerk/themes";
 import { Toaster } from "@/components/ui/sonner";
 import ClerkClientProvider from "@/components/ClerkClientProvider";
 
-
-
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -20,24 +17,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <>
-    <ClerkClientProvider >
-      <html lang="en">
-        <body className={`${inter.className} animated-dotted-background`}>
-          <ThemeProvider attribute="class" defaultTheme="dark" > 
-            {/* header */}
-            <Header/>
-            <main className="min-h-screen">{children}</main>
-            <Toaster/>
-            <footer className="py-12 bg-gray-900">
-              <div className=" text-center container mx-auto px-4 ">
-                <div></div>
-                <p>Made by &copy; Asif ahmed sahil - 2024</p>
-              </div>
-            </footer>
-          </ThemeProvider>
-        </body>
-      </html>
-    </ClerkClientProvider>
+      <ClerkClientProvider>
+        <html lang="en">
+          <body className={`${inter.className} animated-dotted-background`}>
+            <ThemeProvider attribute="class" defaultTheme="dark">
+              {/* header */}
+              <Header />
+              <main className="min-h-screen">{children}</main>
+              <Toaster />
+              <footer className="py-12 bg-gray-900">
+                <div className=" text-center container mx-auto px-4 ">
+                  <p>Made by &copy; Asif ahmed sahil - 2024</p>
+                </div>
+              </footer>
+            </ThemeProvider>
+          </body>
+        </html>
+      </ClerkClientProvider>
     </>
   );
 }
