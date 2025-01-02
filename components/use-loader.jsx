0718e8d1387896384c5a8f7b68bdda052a6dc0d5
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-01-02 21:56:57 +0600

    update

--- components/use-loader.jsx
+++ components/use-loader.jsx
@@ -8,7 +8,7 @@ const UseLoading = () => {
   const { isLoaded: isUserLoaded } = useUser();
 
   if (!isLoaded || !isUserLoaded) {
-    return <BarLoader className="mb-4 " width={"100%"} color="#36d7b7"/>
+    return <BarLoader className="mb-4 " width={"100%"} color="#36d7b7"/> update
   } 
 };
 
